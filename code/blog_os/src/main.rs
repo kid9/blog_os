b85e1d84ff3ec9a9d078bbd67aa1d183c27588af
Geng.Zheng <Geng.Zheng@linux.alibaba-inc.com>
2022-07-04 17:30:10 +0800

    add impl for vga text

--- code/blog_os/src/main.rs
+++ code/blog_os/src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
-
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+     println!("{}", _info);
      loop{}
 }
 
@@ -12,17 +14,14 @@ static HELLO: &[u8] = b"Hello world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-     let vga_buffer = 0xb8000 as *mut u8;
-     for (i, &byte) in HELLO.iter().enumerate() {
-          // we manually set address 0xb8000 as pointer, it may be a invalid address
-          // so unsafe is used to surround the pointer operation
-          unsafe {
-               // set vga ascii byte
-               *vga_buffer.offset(i as isize * 2) = byte;
-               // set vga color byte (light cyan)
-               *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-          }
-     }
+     // vga_buffer::print_something();
+
+     // use core::fmt::Write;
+     // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+     // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+
+     println!("Hello world {}\nthis is new line \nthis is new line again", "!");
      // not exit
+     // panic!("some panic message");
      loop{}
 }
