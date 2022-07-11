a9baf942600c7b7f197fc9e45d9d409ae15484bc
zhaoyan <zhenggeng.zg@alibaba-inc.com>
2022-07-11 16:41:18 +0800

    impl blog 04: add test frameworks

--- code/blog_os/src/main.rs
+++ code/blog_os/src/main.rs
@@ -1,16 +1,24 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
+use blog_os::println;
 
-mod vga_buffer;
-
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-     println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+     println!("{}", info);
      loop{}
 }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+     blog_os::test_panic_handler(info);
+}
 
-static HELLO: &[u8] = b"Hello world!";
+// static HELLO: &[u8] = b"Hello world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -23,5 +31,8 @@ pub extern "C" fn _start() -> ! {
      println!("Hello world {}\nthis is new line \nthis is new line again", "!");
      // not exit
      // panic!("some panic message");
+     #[cfg(test)]
+     test_main();
+
      loop{}
 }
